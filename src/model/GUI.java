543e066c08b6c642baebf6283ba9bb2ce2e049bf
Ignacio Cazcarra <nacho.eze.cazcarra@gmail.com>
2020-07-30 22:57:23 -0300

    Re-organized some code

--- src/model/GUI.java
+++ src/model/GUI.java
@@ -9,14 +9,12 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Deque;
-import java.util.HashSet;
+
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
-import java.util.TreeSet;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,23 +23,24 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-
 public class GUI {
 	JFrame frame;
 	Node[][] map;
 	Map canvas;
 	JPanel panel = new JPanel();
 	int cells = 30;
-	int psx = -1;
-	int psy = -1;
-	int pfx = -1;
-	int pfy = -1;
+	int startx = -1;
+	int starty = -1;
+	int finalx = -1;
+	int finaly = -1;
 	// Botones
 	String[] pinceles = { "Nodo inicial", "Nodo final", "Borrador", "Muro" };
-	//Enum
-	enum Cells{
-		START,FINISH,EMPTY,WALL,VISITED,PATH
+
+	// Enum
+	enum Cells {
+		START, FINISH, EMPTY, WALL, VISITED, PATH
 	}
+
 	// Constantes
 	private int WIDTH = 700;
 	private int HEIGHT = 630;
@@ -57,7 +56,6 @@ public class GUI {
 			createEmptyMap();
 			initialize();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -79,42 +77,43 @@ public class GUI {
 		valuePanel.add(new JLabel("Ingrese la posicion del nodo inicial"));
 		valuePanel.add(new JLabel(""));
 
-		valuePanel.add(new JLabel("X (entre 1 y " + (cells-1) + "): "));
+		valuePanel.add(new JLabel("X (entre 1 y " + (cells - 1) + "): "));
 		JTextField sx = new JTextField("");
 		valuePanel.add(sx);
-		valuePanel.add(new JLabel("Y (entre 1 y " + (cells-1) + "): "));
+		valuePanel.add(new JLabel("Y (entre 1 y " + (cells - 1) + "): "));
 		JTextField sy = new JTextField("");
 		valuePanel.add(sy);
 
 		valuePanel.add(new JLabel("Ingrese la posicion del nodo final"));
 		valuePanel.add(new JLabel(""));
-		valuePanel.add(new JLabel("X (entre 1 y " + (cells-1) + "): "));
+		valuePanel.add(new JLabel("X (entre 1 y " + (cells - 1) + "): "));
 		JTextField fx = new JTextField("");
 		valuePanel.add(fx);
-		valuePanel.add(new JLabel("Y (entre 1 y " + (cells-1) + "): "));
+		valuePanel.add(new JLabel("Y (entre 1 y " + (cells - 1) + "): "));
 		JTextField fy = new JTextField("");
 		valuePanel.add(fy);
 
 		int result = JOptionPane.showConfirmDialog(null, valuePanel, " ", JOptionPane.OK_CANCEL_OPTION,
 				JOptionPane.PLAIN_MESSAGE);
 
-		if(result==JOptionPane.OK_CANCEL_OPTION) throw new Exception("Operacion cancelada");
-		
-		psx = Integer.parseInt(sx.getText());
-		psy = Integer.parseInt(sy.getText());
+		if (result == JOptionPane.OK_CANCEL_OPTION)
+			throw new Exception("Operacion cancelada");
 
-		pfx = Integer.parseInt(fx.getText());
-		pfy = Integer.parseInt(fy.getText());
+		startx = Integer.parseInt(sx.getText());
+		starty = Integer.parseInt(sy.getText());
 
-		if (psx >= cells || psy >= cells || pfx >= cells || pfy >= cells)
+		finalx = Integer.parseInt(fx.getText());
+		finaly = Integer.parseInt(fy.getText());
+
+		if (startx >= cells || starty >= cells || finalx >= cells || finaly >= cells)
 			throw new Exception("ERROR: Por lo menos uno de los valores ingresados excede la capacidad maxima");
-		if (psx < 0 || psy < 0 || pfx < 0 || pfy < 0)
+		if (startx < 0 || starty < 0 || finalx < 0 || finaly < 0)
 			throw new Exception("ERROR: Por lo menos uno de los valores ingresados es menor a la posicion minima (0)");
-		if (psx == pfx && psy == pfy)
+		if (startx == finalx && starty == finaly)
 			throw new Exception("ERROR: Los puntos no pueden tener las mismas coordenadas");
 
-		map[psx][psy].setCellType(Cells.START);
-		map[pfx][pfy].setCellType(Cells.FINISH);
+		map[startx][starty].setCellType(Cells.START);
+		map[finalx][finaly].setCellType(Cells.FINISH);
 	}
 
 	public void initialize() throws Exception {
@@ -138,12 +137,13 @@ public class GUI {
 	}
 
 	class Node {
-		
+
 		private Cells cellType;
 		private int x;
 		private int y;
 		private boolean visited = false;
 		private Node prev;
+
 		public Node(Cells cellType, int x, int y) {
 			super();
 			this.cellType = cellType;
@@ -198,6 +198,62 @@ public class GUI {
 				return false;
 			return true;
 		}
+
+		public int f() {
+			return g() + h();
+		}
+
+		public int g() {
+			return findShortestPath().size()-1;
+		}
+
+		public int h() {
+			return Math.abs((this.getX() - finalx)) + Math.abs((this.getY() - finaly));
+		}
+		
+		public List<Node> getNeighbours() {
+		
+			List<Node> neighbours = new ArrayList<Node>();
+			int row = this.getX(); 
+			int col = this.getY();
+			
+			if (row - 1 >= 0 && (map[row - 1][col].getCellType().equals(Cells.EMPTY)
+					|| map[row - 1][col].getCellType().equals(Cells.FINISH))) {
+				neighbours.add(map[row - 1][col]);
+			}
+			if (col - 1 >= 0 && (map[row][col - 1].getCellType().equals(Cells.EMPTY)
+					|| map[row][col - 1].getCellType().equals(Cells.FINISH))) {
+				neighbours.add(map[row][col - 1]);
+			}
+			if (row + 1 < cells && (map[row + 1][col].getCellType().equals(Cells.EMPTY)
+					|| map[row + 1][col].getCellType().equals(Cells.FINISH))) {
+				neighbours.add(map[row + 1][col]);
+			}
+			if (col + 1 < cells && (map[row][col + 1].getCellType().equals(Cells.EMPTY)
+					|| map[row][col + 1].getCellType().equals(Cells.FINISH))) {
+				neighbours.add(map[row][col + 1]);
+			}
+			return neighbours;
+		}
+		
+		public List<Node> findShortestPath() {
+			List<Node> lista = new ArrayList<Node>();
+			Node current = this;
+			
+			lista.add(current);
+			while (!current.getCellType().equals(Cells.START)) {
+				current = current.prev;
+				lista.add(current);
+			}
+			return lista;
+		}
+
+		public void setNeighboursPrev() {
+			for (Node n : getNeighbours()) {
+				n.setPrev(this);
+			}
+		}
+		
 	}
 
 	class Map extends JPanel implements MouseListener, MouseMotionListener {
@@ -241,7 +297,7 @@ public class GUI {
 		public void paintWalls(MouseEvent arg0) {
 			int x = arg0.getX() / CSIZE;
 			int y = arg0.getY() / CSIZE;
-			if (!((x == psx && y == psy) || (x == pfx && y == pfy))) {
+			if (!((x == startx && y == starty) || (x == finalx && y == finaly))) {
 				if (map[x][y].getCellType().equals(Cells.EMPTY)) {
 					map[x][y].setCellType(Cells.WALL);
 				}
@@ -253,7 +309,6 @@ public class GUI {
 			try {
 				Thread.sleep(ms);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			SwingUtilities.invokeLater(() -> canvas.repaint());
@@ -261,24 +316,21 @@ public class GUI {
 
 		@Override
 		public void mouseDragged(MouseEvent arg0) {
-			// TODO Auto-generated method stub
 			new Thread(() -> paintWalls(arg0)).start();
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-
 		}
 
 		@Override
-		public void mouseClicked(MouseEvent arg0){
+		public void mouseClicked(MouseEvent arg0) {
 			Algorithms a = new Algorithms();
-			
+
 			try {
-				new Thread(() -> a.DFS(psx, psy)).start();
+				new Thread(() -> a.DFS(startx, starty)).start();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -286,54 +338,52 @@ public class GUI {
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-
 		}
 
 		@Override
 		public void mouseExited(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-
 		}
 
 		@Override
 		public void mousePressed(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-
 		}
 
 	}
 
 	class Algorithms {
 
-		public Algorithms() {
-		};
+		public Algorithms() {}
 
 		public List<Node> DFS(int row, int col) {
+			List<Node> neighbours = map[row][col].getNeighbours();
 
-			List<Node> neighbours = getNeighbours(row, col);
 			for (Node n : neighbours) {
-				if (n.getCellType().equals(Cells.FINISH)) {
-					return findShortestPath(map[row][col]);
-				}
-				if (!n.isVisited() && n.getCellType().equals(Cells.EMPTY)) {
-					n.setCellType(Cells.VISITED);
-					n.setPrev(map[row][col]);
-					canvas.paintWithDelay(10);
-					List<Node> spath = null;
-					spath = DFS(n.getX(), n.getY());
-					if(!spath.isEmpty()) {
-						paintPath(spath);
-						break;
+				if (!map[finalx][finaly].isVisited()) {
+					if (n.getCellType().equals(Cells.FINISH)) {
+						n.setVisited(true);
+						return map[row][col].findShortestPath();
+					}
+					if (!n.isVisited() && n.getCellType().equals(Cells.EMPTY)) {
+						n.setPrev(map[row][col]);
+						n.setCellType(Cells.VISITED);
+						canvas.paintWithDelay(10);
+
+						List<Node> spath = null;
+						spath = DFS(n.getX(), n.getY());
+						if (spath != null && !spath.isEmpty()) {
+							paintPath(spath);
+							break;
+						}
 					}
 				}
 			}
-			
 			return null;
 		}
 
@@ -343,13 +393,15 @@ public class GUI {
 			q.add(map[row][col]);
 			boolean found = false;
 
-			while (!found) {
+			while (!found && !q.isEmpty()) {
 				Node current = q.remove();
 
-				List<Node> s = getNeighbours(current.getX(), current.getY());
+				List<Node> s = current.getNeighbours();
 				q.addAll(s);
+				
 				q = removeDuplicates(q);
-				setPrev(s,current);
+				
+				current.setNeighboursPrev();
 				
 				if (!current.isVisited() && current.getCellType().equals(Cells.EMPTY)) {
 					current.setCellType(Cells.VISITED);
@@ -357,59 +409,26 @@ public class GUI {
 					canvas.paintWithDelay(5);
 				} else if (current.getCellType().equals(Cells.FINISH)) {
 					found = true;
-					paintPath(findShortestPath(current));
+					paintPath(current.findShortestPath());
 					q.clear();
 				}
 			}
 		}
-		
-		
+
 		public void paintPath(List<Node> path) {
 			Collections.reverse(path);
-			for(Node n : path) {
-				if(n.getCellType().equals(Cells.VISITED)) n.setCellType(Cells.PATH);
+			for (Node n : path) {
+				if (n.getCellType().equals(Cells.VISITED))
+					n.setCellType(Cells.PATH);
 				canvas.paintWithDelay(15);
 			}
 		}
-		
-		public List<Node> findShortestPath(Node current) {
-			List<Node> lista = new ArrayList<Node>();
-			while(!current.getCellType().equals(Cells.START)) {
-				current = current.prev;
-				lista.add(current);
-			}
-			return lista;
-		}
-		
-		public Queue<Node> removeDuplicates(Queue<Node> q){
+
+		public Queue<Node> removeDuplicates(Queue<Node> q) {
 			Set<Node> aux = new LinkedHashSet<Node>(q);
 			q.clear();
 			q.addAll(aux);
 			return q;
-		}
-		
-		public void setPrev(List<Node> s, Node current) {
-			for(Node n : s) {
-				n.setPrev(current);
-			}
-		}
-		
-		public List<Node> getNeighbours(int row, int col) {
-			List<Node> neighbours = new ArrayList<Node>();
-
-			if (row - 1 >= 0 && (map[row - 1][col].getCellType().equals(Cells.EMPTY) || map[row - 1][col].getCellType().equals(Cells.FINISH))) {
-				neighbours.add(map[row - 1][col]);	
-			}
-			if (col - 1 >= 0 && (map[row][col - 1].getCellType().equals(Cells.EMPTY) || map[row][col - 1].getCellType().equals(Cells.FINISH))) {
-				neighbours.add(map[row][col - 1]);
-			}
-			if (row + 1 < cells && (map[row + 1][col].getCellType().equals(Cells.EMPTY) || map[row + 1][col].getCellType().equals(Cells.FINISH))) {
-				neighbours.add(map[row + 1][col]);
-			}
-			if (col + 1 < cells && (map[row][col + 1].getCellType().equals(Cells.EMPTY) || map[row][col + 1].getCellType().equals(Cells.FINISH))) {
-				neighbours.add(map[row][col + 1]);
-			}
-			return neighbours;
 		}
 
 	}
